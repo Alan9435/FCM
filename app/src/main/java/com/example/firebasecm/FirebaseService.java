1dc222590655597f72e63732df4b3aec0ec3823a
s990611 <s29139161@gmail.com>
2021-05-01 13:50:42 +0800

    add Notification

--- app/src/main/java/com/example/firebasecm/FirebaseService.java
+++ app/src/main/java/com/example/firebasecm/FirebaseService.java
@@ -1,9 +1,22 @@
 package com.example.firebasecm;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Message;
 import android.util.Log;
 
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 
 public class FirebaseService extends FirebaseMessagingService {
     //接收 收到的訊息
@@ -14,15 +27,51 @@ public class FirebaseService extends FirebaseMessagingService {
         if (remoteMessage.getNotification() != null) {
             Log.i("***","title "+remoteMessage.getNotification().getTitle());
             Log.i("***","body "+remoteMessage.getNotification().getBody());
-        }
 
+            sendNotification(remoteMessage.getNotification().getTitle(),
+                    remoteMessage.getNotification().getBody());
+        }
     }
     //會產一個手機裝置的 token 傳送訊息識別用
     @Override
     public void onNewToken(String token) {
         super.onNewToken(token);
         Log.i("***","token "+token);
+        /*，註冊令牌可能會更改：
+                該應用已在新設備上還原
+                用戶卸載/重新安裝應用程序
+                用戶清除應用程序數據*/
     }
 
+    private void sendNotification(String messageTitle, String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String channelId = "default_notification_channel_id";
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setContentTitle(messageTitle)
+                        .setSmallIcon(R.drawable.ic_android_black_24dp)
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
 }
 
--- app/src/main/java/com/example/firebasecm/MainActivity.java
+++ app/src/main/java/com/example/firebasecm/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.firebasecm;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCanceledListener;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.iid.FirebaseInstanceId;
@@ -16,11 +22,41 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        String channelId  = "default_notification_channel_id";
+        String channelName = "default_notification_channel_name";
+        NotificationManager notificationManager =
+                getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(new NotificationChannel(channelId,
+                channelName, NotificationManager.IMPORTANCE_LOW));
+
+        FirebaseMessaging.getInstance().subscribeToTopic("weather")     //訂閱主題
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = getString(R.string.msg_subscribed);
+                        if (!task.isSuccessful()) {
+                            msg = getString(R.string.msg_subscribe_failed);
+
+                        }
+                        Log.d("***", msg);
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+//        FirebaseMessaging.getInstance().unsubscribeFromTopic("weather"); //取消訂閱
+
+
+//        SendBackgroundTest();
+    }
+
+    private void SendBackgroundTest() {
         FirebaseMessaging.getInstance().getToken()
                 .addOnCompleteListener(new OnCompleteListener<String>() {
                     @Override
@@ -36,8 +72,5 @@ public class MainActivity extends AppCompatActivity {
                         Log.d("***", token);
                     }
                 });
-
     }
-
-
 }
